8944bf7902a1be6c85f5b375b9ab7315843f4e80
Creshent <dobao98123@gmail.com>
2023-03-14 13:36:01 -0400

    Creating the skeleton of DataWriter class

---
+++ src/DataWriter.java
@@ -0,0 +1,34 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class DataWriter extends DataConstants {
+    /**
+     *  Saves users to a json file.
+     */
+    public static void saveUsers() {
+        ArrayList<User> userList = users.getUsers();
+        JSONArray jsonUsers = new JSONArray();
+
+        // creating all the json objects
+        for (int i = 0; i < userList.size(); i++) {
+            jsonUsers.add(getUserJSON(userList.get(i)));
+        }
+
+        // Write JSON file
+        try (FileWriter file = new FileWriter(USER_FILE_NAME)) {
+
+            file.write(jsonUsers.toJSONString());
+            file.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveCourses() {
+
+    }
+}
