73e24ce0764e1723dff25648a2ef9a963791daa9
Vansh Nagpal <2002vansh@gmail.com>
2023-03-14 14:10:30 -0400

    v1 dataloder getUsers method

--- src/DataLoader.java
+++ src/DataLoader.java
@@ -4,6 +4,9 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import java.util.UUID;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class DataLoader extends DataConstants {
   public static ArrayList<User> getUsers() {
@@ -11,37 +14,45 @@ public class DataLoader extends DataConstants {
     // User instance for each entry, then push to an array list
     ArrayList<User> users = new ArrayList<User>();
     try {
-			FileReader reader = new FileReader("json/Users.json");
-			JSONParser parser = new JSONParser();
-			JSONArray usersJSON = (JSONArray)new JSONParser().parse(reader);
-			
-			for(int i=0; i < usersJSON.size(); i++) {
-				JSONObject userJSONObject = (JSONObject)usersJSON.get(i);
-				UUID id = UUID.fromString((String)userJSONObject.get(ID));
-        String userName = (String)userJSONObject.get(USER_NAME);
-        String firstName = (String)userJSONObject.get(FIRST_NAME);
-        String lastName = (String)userJSONObject.get(LAST_NAME);
-        String email = (String)userJSONObject.get(EMAIL); 
-        String password = (String)userJSONObject.get(PASSWORD);
-        Date dateOfBirth = parseDate(userJSONObject.get(DOB_DATE));
-        String userType = (String)userJSONObject.get(TYPE);
-				
-				users.add(new Person(firstName, lastName, phoneNumber));
-			}
-			
-			return people;
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		return null;
-	}
+      FileReader reader = new FileReader("json/Users.json");
+      JSONParser parser = new JSONParser();
+      JSONArray usersJSON = (JSONArray) new JSONParser().parse(reader);
 
-  return null;
+      for (int i = 0; i < usersJSON.size(); i++) {
+        JSONObject userJSONObject = (JSONObject) usersJSON.get(i);
+        UUID id = UUID.fromString((String) userJSONObject.get(ID));
+        String userName = (String) userJSONObject.get(USER_NAME);
+        String firstName = (String) userJSONObject.get(FIRST_NAME);
+        String lastName = (String) userJSONObject.get(LAST_NAME);
+        String email = (String) userJSONObject.get(EMAIL);
+        String password = (String) userJSONObject.get(PASSWORD);
+        Date dateOfBirth = parseDate((String) userJSONObject.get(DOB_DATE));
+        String userType = (String) userJSONObject.get(TYPE);
 
+        users.add(new User(firstName, lastName, phoneNumber));
+      }
+
+      return users;
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return null;
   }
 
   public static ArrayList<Course> getCourses() {
     return null;
   }
+
+  private static Date parseDate(String dob) {
+    SimpleDateFormat dateFormat = new SimpleDateFormat("MMddyyyy");
+    Date date = null;
+    try {
+      date = dateFormat.parse(dob);
+    } catch (ParseException e) {
+      e.printStackTrace();
+    }
+    return date;
+  }
+}
