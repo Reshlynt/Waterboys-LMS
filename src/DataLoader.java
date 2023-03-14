d162fc9584ba98ff37fb8852c8b766bf42143201
Vansh Nagpal <2002vansh@gmail.com>
2023-03-14 13:44:02 -0400

    minor changes

--- src/DataLoader.java
+++ src/DataLoader.java
@@ -1,12 +1,43 @@
 import java.util.ArrayList;
+import java.io.FileReader;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import java.util.UUID;
 
-public class DataLoader {
+public class DataLoader extends DataConstants {
   public static ArrayList<User> getUsers() {
     // purpose of this method is to read Users.json, go through entries, create a
     // User instance for each entry, then push to an array list
     ArrayList<User> users = new ArrayList<User>();
+    try {
+			FileReader reader = new FileReader("json/Users.json");
+			JSONParser parser = new JSONParser();
+			JSONArray usersJSON = (JSONArray)new JSONParser().parse(reader);
+			
+			for(int i=0; i < usersJSON.size(); i++) {
+				JSONObject personJSON = (JSONObject)usersJSON.get(i);
+				UUID id = UUID.fromString((String)personJSON.get(ID));
+        String userName = (String)personJSON.get(USER_NAME);
 
-    return null;
+        
+				String lastName = (String)personJSON.get(PEOPLE_LAST_NAME);
+				String phoneNumber = (String)personJSON.get(PEOPLE_PHONE_NUMBER);
+				
+				users.add(new Person(firstName, lastName, phoneNumber));
+			}
+			
+			return people;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+
+  return null;
+
   }
 
   public static ArrayList<Course> getCourses() {
