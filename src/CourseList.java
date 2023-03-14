9e2774aede6cd4e1fbe07f2bd5d0593809025837
Stewys <adamstewart_7@outlook.com>
2023-03-14 13:34:53 -0400

    courselist first stubs

---
+++ src/CourseList.java
@@ -0,0 +1,31 @@
+package src;
+import java.util.ArrayList;
+
+public class CourseList {
+    private static ArrayList<Course> courses = new ArrayList<Course>();
+
+    private CourseList() {
+    }
+
+    public void addCourse(Course course) {
+        courses.add(course);
+    }
+
+    public void deleteCourse(Course course) {
+        courses.remove(course);
+    }
+
+    public ArrayList<Course> getAllCourses() {
+        return courses;
+    }
+
+    public ArrayList<Course> getCourseByKeyword(String name) {
+        ArrayList<Course> result = new ArrayList<Course>();
+        for (Course course : courses) {
+            if (course.getName().contains(name)) {
+                result.add(course);
+            }
+        }
+        return result;
+    }
+}
