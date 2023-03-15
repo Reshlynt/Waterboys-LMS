1ec2b2522304f25f0a22323747b9cf5aa81dfabd
Scott Do <dobao98123@gmail.com>
2023-03-14 21:30:54 -0400

    Removed package src that came with merge branch of main

--- src/DataConstants.java
+++ src/DataConstants.java
@@ -1,4 +1,4 @@
-package src;
+
 public class DataConstants {
   // Course Constants
   public static final String TITLE = "title";
--- src/LMSSystem.java
+++ src/LMSSystem.java
@@ -1,4 +1,4 @@
-package src;
+
 
 public class LMSSystem {
     public UserList userList;
