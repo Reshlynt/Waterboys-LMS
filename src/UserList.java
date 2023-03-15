1f2ef9214cd48ac7f0f65ae1fa6decc49ac07abd
Vansh Nagpal <2002vansh@gmail.com>
2023-03-15 10:41:18 -0400

    fix UserList ctor v1

--- src/UserList.java
+++ src/UserList.java
@@ -3,43 +3,44 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 public class UserList {
-    private static ArrayList<User> userList;
-    private UserList() {
-      userList = DataLoader.getUsers();
-    }
-    
-    public static UserList getInstance() {
-        if ( userList == null)  {
-            userList = new ArrayList<User>();
-        }
-        return new UserList();
-    }
+  private static ArrayList<User> userList;
+  private static UserList singletonUserList;
+  private UserList() {
+    userList = DataLoader.getUsers();
 
-    /**
-     * 
-     * @return Instance of the UserList object.
-     */
+  }
 
-    public void addUser(User user) {
-        userList.add(user);
+  public static UserList getInstance() {
+    if (singletonUserList == null) {
+      singletonUserList = new UserList();
     }
+    return singletonUserList;
+  }
 
-    public void deleteUser(User user) {
-        userList.remove(user);
-    }
+  /**
+   * 
+   * @return Instance of the UserList object.
+   */
 
-    public ArrayList<User> getUserList() {
-        return userList;
-    }
+  public void addUser(User user) {
+    userList.add(user);
+  }
 
-    public User getUserByUUID(UUID id) {
-        for (User user : userList) {
-            if (user.equals(id)) {
-                return user;
-            }
-        }
-        return null;
+  public void deleteUser(User user) {
+    userList.remove(user);
+  }
+
+  public ArrayList<User> getUserList() {
+    return userList;
+  }
+
+  public User getUserByUUID(UUID id) {
+    for (User user : userList) {
+      if (user.equals(id)) {
+        return user;
+      }
     }
-    
+    return null;
+  }
 
 }
