80ef13e87030f1f29854b0ff1b8be5bca166125a
Vansh Nagpal <2002vansh@gmail.com>
2023-03-15 14:38:26 -0400

    added ctor for Course

--- src/Course.java
+++ src/Course.java
@@ -3,59 +3,75 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 public class Course {
-    private UUID id;
-    private String title;
-    private Difficulty difficulty;
-    private String description;
-    private Teacher author;
-    private Assessment exam;
-    private CourseType coursetype;
-    private ArrayList<Module> lessons;
-    private ArrayList<Comment> courseComments;
-    
-    public boolean displayComments() {
-        return true;
-    }
-    public void displaySlides() {
-        return;
-    }
-    public Certificate createCertificate() {
-        return new Certificate();
-    }
-    public boolean listComment() {
-        return true;
-    }
-    public Difficulty getDifficulty() {
-        return difficulty;
-    }
+  private UUID id;
+  private String title;
+  private Difficulty difficulty;
+  private String description;
+  private Teacher author;
+  private Assessment exam;
+  private CourseType courseType;
+  private ArrayList<Module> lessons;
+  private ArrayList<Comment> courseComments;
 
-    // Returns the course's UUID
-    public UUID getID() {
-        return id;
-    }
+  public Course(UUID id, String title, Difficulty difficulty, String description, Teacher author, Assessment exam,
+      CourseType courseType, ArrayList<Module> lessons, ArrayList<Comment> courseComments) {
+    this.id = id;
+    this.title = title;
+    this.difficulty = difficulty;
+    this.description = description;
+    this.author = author;
+    this.courseType = courseType;
+    this.lessons = lessons;
+    this.courseComments = courseComments;
+  }
 
-    // Returns the course's title
-    public String getTitle() {
-        return title;
-    }
+  public boolean displayComments() {
+    return true;
+  }
 
-    // Return course description
-    public String getDescription() {
-        return description;
-    }
+  public void displaySlides() {
+    return;
+  }
 
-    //Return course author.
-    public Teacher getAuthor() {
-        return author;
-    }
+  public Certificate createCertificate() {
+    return new Certificate();
+  }
 
-    // Return course type
-    public CourseType getCourseType() {
-        return coursetype;
-    }
+  public boolean listComment() {
+    return true;
+  }
 
-    // Return course assessment
-    public Assessment getAssessment() {
-        return exam;
-    }
+  public Difficulty getDifficulty() {
+    return difficulty;
+  }
+
+  // Returns the course's UUID
+  public UUID getID() {
+    return id;
+  }
+
+  // Returns the course's title
+  public String getTitle() {
+    return title;
+  }
+
+  // Return course description
+  public String getDescription() {
+    return description;
+  }
+
+  // Return course author.
+  public Teacher getAuthor() {
+    return author;
+  }
+
+  // Return course type
+  public CourseType getCourseType() {
+    return coursetype;
+  }
+
+  // Return course assessment
+  public Assessment getAssessment() {
+    return exam;
+  }
 }
