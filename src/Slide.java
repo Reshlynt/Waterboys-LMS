3661a9c8a791d7148c90c5a530e460bc206f00c2
Vansh Nagpal <2002vansh@gmail.com>
2023-03-14 20:46:47 -0400

    changed slide class to have a constructor

--- src/Slide.java
+++ src/Slide.java
@@ -1,9 +1,19 @@
 package src;
+
 public class Slide {
-    public String toString() {
-        return "";
-    }
-    public void display() {
-        return;
-    }
+  private String title;
+  private String content;
+
+  public Slide(String title, String content) {
+    this.title = title;
+    this.content = content;
+  }
+
+  public String toString() {
+    return "";
+  }
+
+  public void display() {
+    return;
+  }
 }
