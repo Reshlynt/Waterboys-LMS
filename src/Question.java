60bff92e3077c31ba78433e4b8786e98cd2938ee
Vansh Nagpal <2002vansh@gmail.com>
2023-03-15 16:25:12 -0400

    added ctor

--- src/Question.java
+++ src/Question.java
@@ -1,11 +1,23 @@
 package src;
+
 import java.util.ArrayList;
 
 public class Question {
-    public boolean createQuestion(ArrayList<Integer> answers, String prompt, int correctAnswer) {
-        return true;
-    }
-    public Question getQuestion() {
-        return new Question();
-    }
+  String question;
+  ArrayList<String> answerChoices = new ArrayList<String>();
+  String correctAnswer;
+
+  public Question(String question, ArrayList<String> answerChoices, String correctAnswer) {
+    this.question = question;
+    this.answerChoices = answerChoices;
+    this.correctAnswer = correctAnswer;
+  }
+
+  public boolean createQuestion(ArrayList<String> answers, String prompt, String correctAnswer) {
+    return true;
+  }
+
+  public Question getQuestion() {
+    return new Question();
+  }
 }
