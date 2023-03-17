14cdbf30e789dbd3677534ea9ad2bdf353acc602
Vansh Nagpal <2002vansh@gmail.com>
2023-03-17 15:04:51 -0400

    refactoring

--- src/UserList.java
+++ src/UserList.java
@@ -2,61 +2,64 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 public class UserList {
-    private static ArrayList<User> userList;
-    private static UserList singletonUserList;
+  private static ArrayList<User> userList;
+  private static UserList singletonUserList;
 
-    public UserList() {
-        userList = DataLoader.getUsers();
-    }
+  public UserList() {
+    userList = DataLoader.getUsers();
+  }
 
-    public static UserList getInstance() {
-        if (singletonUserList == null)
-            singletonUserList = new UserList();
-        return singletonUserList;
-    }
+  public static UserList getInstance() {
+    if (singletonUserList == null)
+      singletonUserList = new UserList();
+    return singletonUserList;
+  }
 
-    /**
-    * 
-    * @return Instance of the UserList object.
-    */
+  /**
+   * 
+   * @return Instance of the UserList object.
+   */
 
-    public void addUser(User user) {
-        userList.add(user);
-    }
-  
-    public void deleteUser(User user) {
-        userList.remove(user);
-    }
+  public void addUser(User user) {
+    if (!userList.contains((user)))
+      userList.add(user);
+  }
 
-    public ArrayList<User> getUserList() {
-        return userList;
-    }
+  public void deleteUser(User user) {
+    userList.remove(user);
+  }
 
-    public User getUserByUUID(UUID id) {
-        for (User user : userList) {
-            if (user.equals(id)) {
-                return user;
-            }
-        }
-        return null;
-    }
+  public ArrayList<User> getUserList() {
+    return userList;
+  }
 
-    public boolean foundUser(String username) {
-        for (User user : userList) {
-            if (user.getUsername().equals(username)) {
-                return true;
-            }
-        }
-        return false;
+  public User getUserByUUID(UUID id) {
+    System.out.print("userList size ");
+    System.out.println(userList.size());
+    for (User user : userList) {
+      if (user.equals(id)) {
+        return user;
+      }
     }
+    return null;
+  }
 
-    public User getUser(String username) {
-        for (User user : userList) {
-            if (user.getUsername().equals(username)) {
-                return user;
-            }
-        }
-        return null;
+  public boolean foundUser(String username) {
+    for (User user : userList) {
+      if (user.getUsername().equals(username)) {
+        return true;
+      }
     }
+    return false;
+  }
+
+  public User getUser(String username) {
+    for (User user : userList) {
+      if (user.getUsername().equals(username)) {
+        return user;
+      }
+    }
+    return null;
+  }
 
 }
