91d86ea1ade641ad71bb5763e9515848aa414b3e
jagger tanner <jrtanner@email.sc.edu>
2023-03-19 19:38:30 -0400

    added printComments

--- src/Module.java
+++ src/Module.java
@@ -5,7 +5,7 @@ public class Module {
   private ArrayList<TextSlide> slides;
   private ArrayList<Comment> comments;
   private Assessment lessonQuiz;
-
+  // Ask portia if we should add Comment IDs for replying
   public Module(String title, ArrayList<TextSlide> slides, ArrayList<Comment> comments, Assessment lessonQuiz) {
     this.title = title;
     this.slides = slides;
@@ -42,6 +42,14 @@ public class Module {
   // returns the module's comments
   public ArrayList<Comment> getComments() {
     return comments;
+  }
+  public void printComments(){
+    for(int i = 0; i<comments.size();i++){
+      System.out.println(comments.get(i));
+      for (int j = 0; j < comments.get(i).getComments().size(); j++) {
+        System.out.println(comments.get(i).getComments().get(j));
+      }
+    }
   }
   // returns the module's quiz
   public Assessment getQuiz() {
