6a17afe1f35181ca1bccfa0410aae3f40d024553
Scott Do <dobao98123@gmail.com>
2023-03-24 19:33:08 -0400

    Create method to present consoel text looking left-aligned

--- src/TextSlide.java
+++ src/TextSlide.java
@@ -1,27 +1,61 @@
 package src;
+
 public class TextSlide implements Slide {
-    private String title;
-    private String contents;
-    public TextSlide(String title, String contents){
-      this.title = title;
-      this.contents = contents;
-    }
-    public void display() {
-      System.out.println("|\n|\t" + title + "\n|--------------------------------------------------------------------------------------\n\n");
-      System.out.println("\t\t" + contents);
-      // TODO: if the text is too long, split into multiple lines    
-      System.out.println("|\n|--------------------------------------------------------------------------------------\n");
+
+  private String title;
+
+  private String contents;
+
+  public TextSlide(String title, String contents) {
+    this.title = title;
+    this.contents = contents;
+    formatText();
+
+  }
+
+  public void display() {
+    System.out.println("|\n|\t" + title
+        + "\n|--------------------------------------------------------------------------------------\n\n");
+    System.out.println("\t\t" + contents);
+    // TODO: if the text is too long, split into multiple lines
+    System.out.println(
+        "|\n|--------------------------------------------------------------------------------------\n");
+    return;
+  }
+
+  public String toString() {
+    return "TEXT SLIDE: " + title + ":\n" + contents;
+  }
+
+  // returns the slide's title
+  public String getTitle() {
+    return title;
+  }
+
+  // returns the slide's contents
+  public String getContents() {
+    return contents;
+  }
+  /**
+   * Using the contents of the slide, this method will replace a ' ' with a newline character every
+   * 87 characters. This is to ensure that the text is not too long to fit on the screen.
+   */
+  public void formatText() {
+    if (contents.length() < 87)
       return;
+    // Prime the loop
+    int i = 74;
+    if (contents.indexOf(' ', i) != -1) {
+      i = contents.indexOf(' ', i);
+      contents = contents.substring(0, i) + "\n" + contents.substring(i + 1, contents.length());
     }
-    public String toString(){
-      return "TEXT SLIDE: " + title + ":\n" + contents;
+    i += 87;
+    while (i < contents.length()) {
+      if (contents.indexOf(' ', i) != -1) {
+        i = contents.indexOf(' ', i);
+        contents = contents.substring(0, i) + "\n" + contents.substring(i + 1, contents.length());
+      }
+      i += 87;
     }
-    // returns the slide's title
-    public String getTitle() {
-      return title;
-    }
-    // returns the slide's contents
-    public String getContents() {
-      return contents;
-    }
+  }
 }
