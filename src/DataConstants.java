69825a41770e449e30f375afd92d25c76b43c204
Creshent <dobao98123@gmail.com>
2023-03-27 10:48:33 -0400

    spaced constants

--- src/DataConstants.java
+++ src/DataConstants.java
@@ -60,10 +60,12 @@ public class DataConstants {
   public static final String REPLIES = "replies";// replies to comments
   public static final String REPLIER_ID = "id";
   public static final String REPLY_TEXT = "text";
+
   // public static final String SECOND_REPLIES = "more_replies";// replies to
   // replies
   // public static final String SECOND_REPLIER_ID = "id";
   // public static final String SECOND_REPLY_TEXT = "text";
+  
   public static final String LIKES = "likes";
   public static final String DISLIKES = "dislikes";
 
