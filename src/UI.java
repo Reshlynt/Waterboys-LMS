8353af346a2a4835251fb68689868fa4984d67e1
PushpendraSShekhawat <pushpendra.shekhawat96@gmail.com>
2023-03-30 14:30:53 -0400

    done

--- src/UI.java
+++ src/UI.java
@@ -890,7 +890,7 @@ public class UI {
             grade = ((Student) user).getCourseGradeList(course).get(i-1).toString();
           }
         }
-        WelcomeLine5(10, (num + ".) " + modules.get(i).getTitle() + " "
+        WelcomeLine5(10, (num + ".) " + modules.get(i).getTitle() + "\t"
                                        + grade + "\n"));
         num++;
         grade = "";
@@ -938,21 +938,21 @@ public class UI {
               int[] count = {0, 0, 0};
               for (Comment comment : comments) {
                 count[0]++;
-                System.out.println(count[0] + " " + comment.getPostingUser().getFullName());
+                System.out.println(count[0] + " " + comment.getPostingUser().getUsername());
                 System.out.println("\t\"" + comment.getPost() + "\"");
                 if (comment.getReplies().size() !=0) {
                   ArrayList<Comment> replies = comment.getReplies();
                   for (Comment reply : replies) {
                     count[1]++;
                     System.out.println("\t|");
-                    System.out.println((count[0] + count[1]) + " \t| " + reply.getPostingUser().getFullName());
+                    System.out.println((count[0] + count[1]) + " \t| " + reply.getPostingUser().getUsername());
                     System.out.println("\t\t\"" + reply.getPost() + "\"");
                     if (reply.getReplies().size() !=0) {
                       ArrayList<Comment> replies2 = comment.getReplies();
                       for (Comment reply2 : replies2) {
                         count[2]++;
                         System.out.println("\t\t|");
-                        System.out.println((count[0] + count[1] + count[2]) + " \t\t| " + reply2.getPostingUser().getFullName());
+                        System.out.println((count[0] + count[1] + count[2]) + " \t\t| " + reply2.getPostingUser().getUsername());
                         System.out.println("\t\t\t\"" + reply2.getPost() + "\"");
                       }
                     }
