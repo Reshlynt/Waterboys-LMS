d5ab83a6ab0fb8d807622fdf676a6a58782b1943
PushpendraSShekhawat <pushpendra.shekhawat96@gmail.com>
2023-03-30 20:14:59 -0400

    Users can Reply!

--- src/UI.java
+++ src/UI.java
@@ -33,7 +33,7 @@ public class UI {
           user = SignUp();
           UserList userList = UserList.getInstance();
           userList.addUser(user);
-          UserList.saveUsers();
+          DataWriter.saveUsers();
           break;
         case 9:
           Quit();
@@ -902,6 +902,8 @@ public class UI {
   }
 
   public static Course getCourses(Student user) {
+    // DataWriter.saveCourses();
+    // ArrayList<Course> student_courses = DataLoader.getCourses();
     ArrayList<Course> allCourses = CourseList.getInstance().getCourseList();
     ArrayList<Course> studentCourses = new ArrayList<Course>();
     for (Course course : allCourses) {
@@ -973,8 +975,7 @@ public class UI {
         if (studentCompletedModule) {
           grade = ConsoleColor.CYAN + ((Student) user).getCourseGradeList(course).get(i - 1).toString();
           WelcomeLine5(10, ConsoleColor.GREEN
-              + (num + ".) " + modules.get(i).getTitle() + "\t" + ConsoleColor.RESET + grade + "\n"
-                  + ConsoleColor.RESET));
+              + (num + ".) " + modules.get(i).getTitle() + "\t" + ConsoleColor.RESET+grade + "\n" + ConsoleColor.RESET));
         } else
           WelcomeLine5(10, (num + ".) " + modules.get(i).getTitle() + "\t" + grade + "\n"));
         num++;
@@ -1023,6 +1024,7 @@ public class UI {
               WelcomeLine7("Enter \"Yes\" or \"No\"\n");
               String choice = INPUT.nextLine();
               if (choice.equalsIgnoreCase("yes")) {
+                clearScreen();
                 WelcomeLine7("Tell everyone what you would like to say! (Press Enter when done)\n");
                 Comment new_comment = new Comment(INPUT.nextLine(), user);
                 WelcomeLine7("Is this a post or reply? (Enter \"Reply\" or \"Post\")");
@@ -1031,7 +1033,8 @@ public class UI {
                   module.addComment(INPUT.nextLine(), user);
                   CourseList.saveCourses();
                 } else if (item.equalsIgnoreCase("reply")) {
-                  postReplies(module.getComments(), (Student) user, 1);
+                  postReplies(module.getComments(), (Student) user, 1, new_comment);
+                  CourseList.saveCourses();
                 } else {
                   WelcomeLine7("You entered an invalid choice, moving on...");
                 }
@@ -1072,7 +1075,7 @@ public class UI {
           } else if (value == 3) {
             AccessCourse(course, user);
           } else if (value == 4) {
-            CourseList.writeModule(module);
+            DataWriter.WriteModule(module);
           } else if (value == 5) {
             return;
           } else {
@@ -1128,7 +1131,7 @@ public class UI {
     }
   }
 
-  private static void postReplies(ArrayList<Comment> comments, Student user, int count) {
+  private static void postReplies(ArrayList<Comment> comments, Student user, int count, Comment new_comment) {
     if (comments != null && comments.size() != 0 && count < 3) {
       for (Comment comment : comments) {
         for (int i = 0; i < count; i++)
@@ -1141,16 +1144,16 @@ public class UI {
         WelcomeLine7("Would you like to post here? (Enter \"Yes\" or \"No\")\n");
         String choice = INPUT.nextLine();
         if (choice.equalsIgnoreCase("yes")) {
-          comment.replyToComment(choice, user);
-          CourseList.saveCourses();
+          comment.replyToComment(new_comment.getPost(), new_comment.getPostingUser());
           return;
         } else if (choice.equalsIgnoreCase("no")) {
           WelcomeLine7("Moving on...");
+          clearScreen();
         } else {
           WelcomeLine7("You entered an invalid choice, moving on...");
         }
         if (comment.getReplies() != null && comment.getReplies().size() != 0) {
-          postReplies(comment.getReplies(), user, count + 1);
+          postReplies(comment.getReplies(), user, count + 1, new_comment);
         }
       }
     }
@@ -1183,6 +1186,8 @@ public class UI {
       } else {
         WelcomeLine7("You have finished this module's quiz!");
         WelcomeLine7("You scored " + numCorrect + " out of " + numQuestions + " points!");
+        // add student's grade to their courseProgress for this course
+        // double score = (double) correct / (double) numQuestions;
         student.updateCourseProgress(course, numCorrect, numQuestions);
         System.out
             .println(ConsoleColor.RED + "Current Course Grade: " + student.getCourseGrade(course) + ConsoleColor.RESET);
@@ -1191,10 +1196,11 @@ public class UI {
   }
 
   private static void Quit() {
-    LMS.saveInfo();
     for (int i = 0; i < 32; i++)
       System.out.print(" ");
     System.out.println("Quitting the LMS...");
+    DataWriter.saveUsers();
+    DataWriter.saveCourses();
     System.exit(0);
   }
 
