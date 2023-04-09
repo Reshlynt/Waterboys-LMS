14b83016e4f1d81dcdc1528b9d640fa307ce29a5
Scott Do <dobao98123@gmail.com>
2023-04-08 23:56:40 -0400

    added various tests

--- src/DataWriterTest.java
+++ src/DataWriterTest.java
@@ -44,7 +44,7 @@ public class DataWriterTest {
     // ---------------------------------------------------------
 
     /*
-     * Test 1: Determine whether DataWriter can save a user. The user will be found by UUID.
+     * Determine whether DataWriter can save a user. The user will be found by UUID.
      */
     @Test
     void testWritingOneUser() {
@@ -63,7 +63,7 @@ public class DataWriterTest {
     }
 
     /**
-     * Test 2: Test several users. 5 users will be added, and this test will find if the third user
+     * Test several users. 5 users will be added, and this test will find if the third user
      * appears. This user will be Lucatiel of Mirrah.
      */
     @Test
@@ -87,7 +87,7 @@ public class DataWriterTest {
     }
 
     /**
-     * Test 3: Write an empty user. Determine if DataWriter can write to a JSON file if the user has
+     * Write an empty user. Determine if DataWriter can write to a JSON file if the user has
      * empty strings as a constructor.
      */
     @Test
@@ -98,7 +98,7 @@ public class DataWriterTest {
     }
 
     /**
-     * Test 4: Test zero users. Does DataWriter still function if the singleton UserList has no
+     * Test zero users. Does DataWriter still function if the singleton UserList has no
      * users.
      */
     @Test
@@ -110,16 +110,36 @@ public class DataWriterTest {
     // ---------------------------------------------------------
     // Course Tests
     // ---------------------------------------------------------
+
+    /**
+     * Test for zero courses. Does DataWriter still function if the singleton CourseList has
+     * no courses.
+     */
+    @Test
+    void testWritingZeroCourses() {
+        Courses = DataLoader.getCourses();
+        assertEquals(0, Courses.size());
+    }
+
+    /**
+     * Test writing one course.
+     */
     @Test
     void testWriteOneCourse() {
         Teacher sampleTeacher = new Teacher("Jorumganer_Ahsoka", "Kirby", "Kirbo",
                 "schlitzmaltliqour@example.com", "password123", "04252002");
 
+        ArrayList<String> choices1 = new ArrayList<String>(4);
+        choices1.add("Option A");
+        choices1.add("Option B");
+        choices1.add("Option C");
+        choices1.add("Option D");
+
         // Create an exam
         ArrayList<Question> questions = new ArrayList<>();
-        questions.add(new Question("What is the capital of France?", null, "Paris"));
-        questions.add(new Question("What is the largest ocean on Earth?", null, "Pacific"));
-        Assessment exam = new Assessment("Final Exam", questions);
+        questions.add(new Question("What is the capital of France?", choices1, "Paris"));
+        questions.add(new Question("What is the largest ocean on Earth?", choices1, "Pacific"));
+        Assessment exam = new Assessment("Final Exam", questions, Type.EXAM);
 
         // Create a module with slides
         ArrayList<TextSlide> slides = new ArrayList<>();
@@ -147,5 +167,139 @@ public class DataWriterTest {
         DataWriter.saveCourses();
         assertEquals(course.getID(), DataLoader.getCourses().get(0).getID());
     }
+
+    /**
+     * Write several courses to a json file. Determine if the fourth course is written
+     * correctly.
+     */
+    @Test
+    void testWriteManyCourse() {
+        Teacher sampleTeacher = new Teacher("Jorumganer_Ahsoka", "Kirby", "Kirbo",
+                "schlitzmaltliqour@example.com", "password123", "04252002");
+
+        ArrayList<String> choices1 = new ArrayList<String>(4);
+        choices1.add("Option A");
+        choices1.add("Option B");
+        choices1.add("Option C");
+        choices1.add("Option D");
+
+        // Create an exam
+        ArrayList<Question> questions = new ArrayList<>();
+        questions.add(new Question("What is the capital of France?", choices1, "Paris"));
+        questions.add(new Question("What is the largest ocean on Earth?", choices1, "Pacific"));
+        Assessment exam = new Assessment("Final Exam", questions, Type.EXAM);
+
+        // Create a module with slides
+        ArrayList<TextSlide> slides = new ArrayList<>();
+        slides.add(new TextSlide("Slide 1", "This is the content of slide 1"));
+        slides.add(new TextSlide("Slide 2", "This is the content of slide 2"));
+        Module module = new Module("Module 1", slides);
+
+        // Create some students
+        Student student1 =
+                new Student("jdoe", "John", "Doe", "jdoe@example.com", "password", "06251995");
+        Student student2 =
+                new Student("mike", "Mike", "Smith", "mike@example.com", "password", "03011998");
+
+        // Create a course with the objects created above
+        ArrayList<Module> modules = new ArrayList<>();
+        modules.add(module);
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(student1);
+        students.add(student2);
+        // Making the same declaration of a course, but with differnt UUIDs.
+        Course course1 = new Course(sampleTeacher, "Intro to Programming", Difficulty.INTERMEDIATE,
+                "This course teaches the basics of programming in Java", exam,
+                CourseType.JAVASCRIPT, modules, students);
+        Course course2 = new Course(sampleTeacher, "Intro to Programming", Difficulty.INTERMEDIATE,
+                "This course teaches the basics of programming in Java", exam,
+                CourseType.JAVASCRIPT, modules, students);
+        Course course3 = new Course(sampleTeacher, "Intro to Programming", Difficulty.INTERMEDIATE,
+                "This course teaches the basics of programming in Java", exam,
+                CourseType.JAVASCRIPT, modules, students);
+        Course course4 = new Course(sampleTeacher, "Intro to Programming", Difficulty.INTERMEDIATE,
+                "This course teaches the basics of programming in Java", exam,
+                CourseType.JAVASCRIPT, modules, students);
+        // Adding the same course as it is too much to keep adding unique courses.
+        // They do have differnt UUIDs.
+        Courses.add(course1);
+        Courses.add(course2);
+        Courses.add(course3);
+        Courses.add(course4);
+        DataWriter.saveCourses();
+        assertEquals(course4.getID(), DataLoader.getCourses().get(3).getID());
+    }
+
+    /**
+     * Test writing a null course.
+     */
+    @Test
+    void testWriteNullCourse() {
+        Course course = null;
+        Courses.add(course);
+        DataWriter.saveCourses();
+        assertEquals(null, DataLoader.getCourses().get(0));
+    }
+
+    /**
+     * Test writing an empty course. It will involve most of its needed obejcts having empty
+     * values.
+     */
+    @Test
+    void testWriteCourseWithNullTeacher() {
+        Teacher sampleTeacher = new Teacher("", "", "", "", "", "");
+
+        ArrayList<String> choices1 = new ArrayList<String>(4);
+        choices1.add("");
+        choices1.add("");
+        choices1.add("");
+        choices1.add("");
+
+        // Create an exam
+        ArrayList<Question> questions = new ArrayList<>();
+        questions.add(new Question("", choices1, ""));
+        questions.add(new Question("", choices1, ""));
+        Assessment exam = new Assessment("", questions, Type.EXAM);
+
+        // Create a module with slides
+        ArrayList<TextSlide> slides = new ArrayList<>();
+        slides.add(new TextSlide("", ""));
+        slides.add(new TextSlide("", ""));
+        Module module = new Module("Module 1", slides);
+
+        // Create some students
+        Student student1 = new Student("", "", "", "", "", "");
+        Student student2 = new Student("", "", "", "", "", "");
+
+        // Create a course with the objects created above
+        ArrayList<Module> modules = new ArrayList<>();
+        modules.add(module);
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(student1);
+        students.add(student2);
+        // Making the same declaration of a course, but with differnt UUIDs.
+        Course course1 = new Course(sampleTeacher, "", Difficulty.INTERMEDIATE, "", exam,
+                CourseType.JAVASCRIPT, modules, students);
+
+        Courses.add(course1);
+        DataWriter.saveCourses();
+        assertEquals(course1.getID(), DataLoader.getCourses().get(0).getID());
+    }
+
+    // -------------------------
+    // Other tests
+    // -------------------------
+    /**
+     * Test if DataWriter can write a user to a json file
+     * regardless of any content in the sington Users list.
+     */
+
+    @Test
+    void UserJsonFileExists() {
+        File file = new File("./json/Users.json");
+        assertTrue(file.exists());
+    }
+
+
 
 }
